a20b991b54f04505e8a065353eb50e09d9ec52b8
Carmine Faella <carminefaella5@gmail.com>
2023-02-22 14:54:19 +0100

    Fine lavoro

--- js/main.js
+++ js/main.js
@@ -6,15 +6,17 @@ let prezzoBi = 0.21;
 
 let prezzoTot = numeroDiKm * prezzoBi;
 
-let prezzoMinore = prezzoTot - (prezzoTot * (20/100));
+let prezzoMinore = (prezzoTot - (prezzoTot * (20/100))).toFixed(2);
 
-let prezzoOver = prezzoTot - (prezzoTot * (40/100));
+let prezzoOver = (prezzoTot - (prezzoTot * (40/100))).toFixed(2);
 
 
 if(eta < 18){
     document.getElementById("price").innerHTML = "Il prezzo del tuo biglietto è: " + (prezzoMinore) + "€";
     document.getElementById("discount").innerHTML = "Hai avuto diritto ad uno sconto del 20%";
-} else if{
-    document.getElementById("price").innerHTML = "Il prezzo del tuo biglietto è: " + (prezzoMinore) + "€";
+} else if(eta > 65){
+    document.getElementById("price").innerHTML = "Il prezzo del tuo biglietto è: " + (prezzoOver) + "€";
     document.getElementById("discount").innerHTML = "Hai avuto diritto ad uno sconto del 40%";
+} else{
+    document.getElementById("price").innerHTML = "Il prezzo del tuo biglietto è: " + (prezzoTot) + "€";
 };
